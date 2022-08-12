f789e92b085aff2bcc3f00f646bca282e3b99ab7
Om2rMs <moro.bash97@gmail.com>
2022-08-12 21:56:46 +0300

    Add files

---
+++ src/components/Note/Note.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Note.css'
+import { MdOutlineCancel } from 'react-icons/md'
+
+
+const Note = ({ id, text, date, deleteNote }) => {
+    return (
+        <div className="note">
+            <div className="textarea-container">
+                <textarea
+                    disabled
+                    className="textarea"
+                    value={text}
+                >
+                </textarea>
+            </div>
+            <div className="note-footer">
+                <small className="remain">بتاريخ {date}</small>
+                <MdOutlineCancel
+                    onClick={() => deleteNote(id)}
+                    className="delete-icon"
+                    size="1.6em" />
+            </div>
+        </div>
+
+    )
+}
+
+
+export default Note;
